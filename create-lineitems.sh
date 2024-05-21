199563bdad024b9ff2ed58c92e9141ad73860ee4
Ronen Green <ronen@pubmax.com>
2024-05-21 15:32:29 +0300

    updates

---
+++ create-lineitems.sh
@@ -0,0 +1,8 @@
+line_item_manager create my_config.yml \
+--single-order \
+--settings my_settings.yml \
+--template my_template.yml \
+--private-key-file pubmax-gam-6b55efa207a8.json \
+--network-code 22815767462 \
+--network-name Pubmax \
+# --bidder-code sonobi
